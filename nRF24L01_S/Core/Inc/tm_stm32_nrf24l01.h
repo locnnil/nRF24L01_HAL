ce2d098388effda5cb1c67a24707fb2e146a0ded
Lincoln Wallace <lincoln.almeida@ativasolucoes.com.br>
2022-08-27 10:58:24 -0300

    (fix)library compatibility: using lib generic for all stm32

--- nRF24L01_S/Core/Inc/tm_stm32_nrf24l01.h
+++ nRF24L01_S/Core/Inc/tm_stm32_nrf24l01.h
@@ -1,34 +1,41 @@
 /**
  * @author  Tilen Majerle
  * @email   tilen@majerle.eu
- * @website http://stm32f4-discovery.com
- * @link    http://stm32f4-discovery.com/2014/06/library-17-nrf24l01-stm32f4xx/
- * @version v1.1.1
+ * @website http://stm32f4-discovery.net
+ * @link    http://stm32f4-discovery.net/2015/09/hal-library-25-nrf24l01-for-stm32fxxx/
+ * @version v1.0
  * @ide     Keil uVision
- * @license GNU GPL v3
+ * @license MIT
  * @brief   Library template 
  *	
-@verbatim
+\verbatim
    ----------------------------------------------------------------------
-    Copyright (C) Tilen Majerle, 2015
-    
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    any later version.
-     
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-    
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    Copyright (c) 2016 Tilen Majerle
+
+    Permission is hereby granted, free of charge, to any person
+    obtaining a copy of this software and associated documentation
+    files (the "Software"), to deal in the Software without restriction,
+    including without limitation the rights to use, copy, modify, merge,
+    publish, distribute, sublicense, and/or sell copies of the Software, 
+    and to permit persons to whom the Software is furnished to do so, 
+    subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be
+    included in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
+    AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+    OTHER DEALINGS IN THE SOFTWARE.
    ----------------------------------------------------------------------
-@endverbatim
+\endverbatim
  */
 #ifndef TM_NRF24L01_H
-#define TM_NRF24L01_H 111
+#define TM_NRF24L01_H 100
 
 /* C++ detection */
 #ifdef __cplusplus
@@ -36,13 +43,13 @@ extern "C" {
 #endif
 
 /**
- * @addtogroup TM_STM32F4xx_Libraries
+ * @addtogroup TM_STM32Fxxx_HAL_Libraries
  * @{
  */
 
 /**
  * @defgroup TM_NRF24L01P
- * @brief    nRF24L01+ library for STM32F4xx devices - http://stm32f4-discovery.com/2014/06/library-17-nrf24l01-stm32f4xx/
+ * @brief    nRF24L01+ library for STM32xxx devices - http://stm32f4-discovery.net/2015/09/hal-library-25-nrf24l01-for-stm32fxxx/
  * @{
  *
  * This library allows you to work with nRF24L01+ modules.
@@ -51,8 +58,8 @@ extern "C" {
  * 
  * \par Default pinout
  * 	
-@verbatim
-NRF24L01+	STM32F4xx	DESCRIPTION
+\verbatim
+NRF24L01+	STM32Fxxx	DESCRIPTION
 
 GND			GND			Ground
 VCC			3.3V		3.3V
@@ -62,64 +69,66 @@ SCK			PC10		SCK pin for SPI
 MOSI		PC12		MOSI pin for SPI
 MISO		PC11		MISO pin for SPI
 IRQ			Not used	Interrupt pin. Goes low when active. Pin functionality is active, but not used in library
-@endverbatim 	
+\endverbatim 	
  *
- * IRQ pin is not used in this library, but it's functionality is enabled by this software.
+ * IRQ pin is not used in this library, but its functionality is enabled by this software.
  *
- * You can still set any pin on F4xx to be an external interrupt and handle interrupts from nRF24L01+ module.
+ * You can still set any pin on Fxxx to be an external interrupt and handle interrupts from nRF24L01+ module.
  *
- * The easiest way to that is to use TM EXTI library and attach interrupt functionality to this pin
+ * The easiest way to that is to use @ref TM_EXTI library and attach interrupt functionality to this pin
  * 
  * \par Custom pinout
  *
  * Add lines below in your defines.h file if you want to change default pinout:
  *
-@verbatim
-//Change SPI used. Refer to TM SPI library to check which pins are for SPI
-#define NRF24L01_SPI				SPI3
-#define NRF24L01_SPI_PINS			TM_SPI_PinsPack_2
+\code
 
-//Change CSN pin. This is for SPI communication
-#define NRF24L01_CSN_PORT			GPIOD
-#define NRF24L01_CSN_PIN			GPIO_Pin_7
 
-//Change CE pin. This pin is used to enable/disable transmitter/receiver functionality
-#define NRF24L01_CE_PORT			GPIOD
-#define NRF24L01_CE_PIN				GPIO_Pin_8
-@endverbatim
+
+
+\endcode
  *
  * \par Changelog
  *
-@verbatim
- Versio 1.1.1
-  - June 21, 2015
-  - Fixed buf with pin configuration
-  
- Version 1.1
-  - March 11, 2015
-  - Added support for my new GPIO system
-
- Version 1.0.1
-  - December 14, 2014
-  - Activated all 3 interrupts in NRF24L01+ 
- 
+\verbatim
  Version 1.0
   - First release
-@endverbatim
+\endverbatim
  *
  * \par Dependencies
  *
-@verbatim
- - STM32F4xx
+\verbatim
+ - STM32Fxxx HAL
  - defines.h
  - TM SPI
  - TM GPIO
-@endverbatim
+\endverbatim
  */
-#include "stm32f4xx.h"
+
+//Change SPI used. Refer to TM SPI library to check which pins are for SPI
+#define NRF24L01_SPI				&hspi1
+#define NRF24L01_SPI_PINS			TM_SPI_PinsPack_2
+
+//Change CSN pin. This is for SPI communication
+#define NRF24L01_CSN_PORT			CS1_GPIO_Port
+#define NRF24L01_CSN_PIN			CS1_Pin
+
+//Change CE pin. This pin is used to enable/disable transmitter/receiver functionality
+#define NRF24L01_CE_PORT			CE1_GPIO_Port
+#define NRF24L01_CE_PIN				CE1_Pin
+
+//Define standards
+#define STD_TIMEOUT 				1000
+#define STD_SIZE					sizeof(uint8_t)
+#define MAX_PAYLOAD					32 //@note   Maximal payload size is 32bytes
+
+#include "main.h"
+#include "spi.h"
 #include "defines.h"
-#include "tm_stm32f4_spi.h"
-#include "tm_stm32f4_gpio.h"
+//#include "stm32fxxx_hal.h"
+//#include "defines.h"
+//#include "tm_stm32_spi.h"
+//#include "tm_stm32_gpio.h"
 
 /**
  * @defgroup TM_NRF24L01P_Macros
@@ -146,10 +155,15 @@ IRQ			Not used	Interrupt pin. Goes low when active. Pin functionality is active,
 #endif
 
 /* Pins configuration */
-#define NRF24L01_CE_LOW				TM_GPIO_SetPinLow(NRF24L01_CE_PORT, NRF24L01_CE_PIN)
-#define NRF24L01_CE_HIGH			TM_GPIO_SetPinHigh(NRF24L01_CE_PORT, NRF24L01_CE_PIN)
-#define NRF24L01_CSN_LOW			TM_GPIO_SetPinLow(NRF24L01_CSN_PORT, NRF24L01_CSN_PIN)
-#define NRF24L01_CSN_HIGH			TM_GPIO_SetPinHigh(NRF24L01_CSN_PORT, NRF24L01_CSN_PIN)
+#define NRF24L01_CE_LOW				HAL_GPIO_WritePin(NRF24L01_CE_PORT, NRF24L01_CE_PIN, GPIO_PIN_RESET)
+#define NRF24L01_CE_HIGH			HAL_GPIO_WritePin(NRF24L01_CE_PORT, NRF24L01_CE_PIN, GPIO_PIN_SET)
+#define NRF24L01_CSN_LOW			HAL_GPIO_WritePin(NRF24L01_CSN_PORT, NRF24L01_CSN_PIN, GPIO_PIN_RESET)
+#define NRF24L01_CSN_HIGH			HAL_GPIO_WritePin(NRF24L01_CSN_PORT, NRF24L01_CSN_PIN, GPIO_PIN_SET)
+
+/* Interrupt masks */
+#define NRF24L01_IRQ_DATA_READY     0x40 /*!< Data ready for receive */
+#define NRF24L01_IRQ_TRAN_OK        0x20 /*!< Transmission went OK */
+#define NRF24L01_IRQ_MAX_RT         0x10 /*!< Max retransmissions reached, last transmission failed */
 
 /**
  * @}
@@ -162,9 +176,23 @@ IRQ			Not used	Interrupt pin. Goes low when active. Pin functionality is active,
  */
 
 /**
+ * @brief  Interrupt structure 
+ */
+typedef union _TM_NRF24L01_IRQ_t {
+	struct {
+		uint8_t reserved0:4;
+		uint8_t MaxRT:1;     /*!< Set to 1 if MAX retransmissions flag is set */
+		uint8_t DataSent:1;  /*!< Set to 1 if last transmission is OK */
+		uint8_t DataReady:1; /*!< Set to 1 if data are ready to be read */
+		uint8_t reserved1:1;
+	} F;
+	uint8_t Status;          /*!< NRF status register value */
+} TM_NRF24L01_IRQ_t;
+
+/**
  * @brief  Transmission status enumeration
  */
-typedef enum {
+typedef enum _TM_NRF24L01_Transmit_Status_t {
 	TM_NRF24L01_Transmit_Status_Lost = 0x00,   /*!< Message is lost, reached maximum number of retransmissions */
 	TM_NRF24L01_Transmit_Status_Ok = 0x01,     /*!< Message sent successfully */
 	TM_NRF24L01_Transmit_Status_Sending = 0xFF /*!< Message is still sending */
@@ -173,32 +201,21 @@ typedef enum {
 /**
  * @brief  Data rate enumeration
  */
-typedef enum {
-	TM_NRF24L01_DataRate_2M,  /*!< Data rate set to 2Mbps */
-	TM_NRF24L01_DataRate_1M,  /*!< Data rate set to 1Mbps */
-	TM_NRF24L01_DataRate_250k /*!< Data rate set to 250kbps */
+typedef enum _TM_NRF24L01_DataRate_t {
+	TM_NRF24L01_DataRate_2M = 0x00, /*!< Data rate set to 2Mbps */
+	TM_NRF24L01_DataRate_1M,        /*!< Data rate set to 1Mbps */
+	TM_NRF24L01_DataRate_250k       /*!< Data rate set to 250kbps */
 } TM_NRF24L01_DataRate_t;
 
 /**
  * @brief  Output power enumeration
  */
-typedef enum {
-	TM_NRF24L01_OutputPower_M18dBm,	/*!< Output power set to -18dBm */
-	TM_NRF24L01_OutputPower_M12dBm, /*!< Output power set to -12dBm */
-	TM_NRF24L01_OutputPower_M6dBm,  /*!< Output power set to -6dBm */
-	TM_NRF24L01_OutputPower_0dBm    /*!< Output power set to 0dBm */
+typedef enum _TM_NRF24L01_OutputPower_t {
+	TM_NRF24L01_OutputPower_M18dBm = 0x00,/*!< Output power set to -18dBm */
+	TM_NRF24L01_OutputPower_M12dBm,       /*!< Output power set to -12dBm */
+	TM_NRF24L01_OutputPower_M6dBm,        /*!< Output power set to -6dBm */
+	TM_NRF24L01_OutputPower_0dBm          /*!< Output power set to 0dBm */
 } TM_NRF24L01_OutputPower_t;
-
-/* Clear interrupt flags */
-#define NRF24L01_CLEAR_INTERRUPTS   do { TM_NRF24L01_WriteRegister(0x07, 0x70); } while (0)
-
-/* Gets interrupt status from device */
-#define NRF24L01_GET_INTERRUPTS     TM_NRF24L01_GetStatus()
-
-/* Interrupt masks */
-#define NRF24L01_IRQ_DATA_READY     0x40 /*!< Data ready for receive */
-#define NRF24L01_IRQ_TRAN_OK        0x20 /*!< Transmission went OK */
-#define NRF24L01_IRQ_MAX_RT         0x10 /*!< Max retransmissions reached, last transmission failed */
 
 /**
  * @}
@@ -327,6 +344,22 @@ void TM_NRF24L01_SetRF(TM_NRF24L01_DataRate_t DataRate, TM_NRF24L01_OutputPower_
  * @retval Status register from NRF
  */
 uint8_t TM_NRF24L01_GetStatus(void);
+
+/**
+ * @brief  Reads interrupts from NRF 
+ * @param  *IRQ: Pointer to @ref TM_NRF24L01_IRQ_t where IRQ status will be saved
+ * @retval IRQ status
+ *            - 0: No interrupts are active
+ *            - > 0: At least one interrupt is active
+ */
+uint8_t TM_NRF24L01_Read_Interrupts(TM_NRF24L01_IRQ_t* IRQ);
+
+/**
+ * @brief  Clears interrupt status
+ * @param  None
+ * @retval None
+ */
+void TM_NRF24L01_Clear_Interrupts(void);
 
 /* Private */
 void TM_NRF24L01_WriteRegister(uint8_t reg, uint8_t value);
